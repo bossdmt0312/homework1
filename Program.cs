3fce571ed9dc0d3dcad988992ada7a627621a4b2
bossdmt0312 <122552032+bossdmt0312@users.noreply.github.com>
2023-05-20 00:43:56 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the row number of Pascal's triangle: ");
+        int rowNumber = int.Parse(Console.ReadLine());
+
+        if (rowNumber < 0)
+        {
+            Console.WriteLine("Invalid Pascal's triangle row number.");
+            Console.Write("Enter the row number of Pascal's triangle: ");
+            rowNumber = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Pascal's triangle:");
+        for (int i = 0; i <= rowNumber; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculatePascalValue(i, j) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculatePascalValue(int row, int column)
+    {
+        if (column == 0 || column == row)
+        {
+            return 1;
+        }
+        else
+        {
+            return CalculatePascalValue(row - 1, column - 1) + CalculatePascalValue(row - 1, column);
+        }
+    }
+}
